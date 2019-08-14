61fbfae615f02cc1c12537a872cdb60ccd13ec00
Zachary Ulissi <zulissi@gmail.com>
2019-08-14 06:58:30 -0400

    misc changes to surface_env (correct indexing, add steepest descent/ascent actions, add final local relaxations, add rewards to max barrier height along the path

--- surface_env.py
+++ surface_env.py
@@ -13,16 +13,19 @@ from ase.constraints import FixAtoms
 from ase.optimize import BFGS
 from ase.io import read, write
 from tensorforce.environments import Environment
+from ase.optimize.bfgslinesearch import BFGSLineSearch
 
 
-ACTION_NAMES = ['up', 'down', 'left', 'right', 'forward', 'backward']
+ACTION_NAMES = ['up', 'down', 'left', 'right', 'forward', 'backward', 'steepest_descent', 'steepest_ascent']
 ACTION_DIRECTION = [
     np.array([ 0, 0, 1]), 
     np.array([ 0, 0,-1]), 
     np.array([ 1, 0, 0]), 
     np.array([-1, 0, 0]), 
     np.array([ 0, 1, 0]), 
-    np.array([ 0,-1, 0])
+    np.array([ 0,-1, 0]),
+    np.array([ 0,0., 0]),
+    np.array([ 0,0., 0]),
 ]
 
 class Surface():
@@ -67,13 +70,16 @@ class Surface():
         slab.set_calculator(EMT())
 
         # c =  FixAtoms(indices=[atom.index for atom in slab if atom.index < 32])  #Fix two layers
-        c =  FixAtoms(indices=[atom.index for atom in slab if atom.index < 8])  #Fix two layers
+        c =  FixAtoms(indices=[atom.index for atom in slab if atom.position[2] < np.mean(slab.positions[:,2])])  #Fix two layers
         slab.set_constraint(c)
         
         self.atoms = slab
         self.free_atoms = list(set(range(len(self.atoms))) - 
             set(self.atoms.constraints[0].get_indices()))
-
+        
+        
+        dyn = BFGSLineSearch(atoms=self.atoms, logfile=None)
+        dyn.run(0.1)
         # print(self.free_atoms)
     
     def calculate_energy(self):
@@ -94,22 +100,13 @@ class Surface():
         return min_x, min_y, min_z
 
     def current_positions(self):
+        self.atoms.wrap()
         return self.atoms.positions[self.free_atoms].reshape(-1)
 
     def change_positions(self, idx, move):
-        idx += 8
-        curr_pos = self.current_positions()
-        self.atoms.positions[idx] += move
-        after_pos = self.current_positions()
-        # print("state diff:", np.sum(after_pos - curr_pos))
-        # if self.atoms.positions[idx, 0] >= 16 or self.atoms.positions[idx, 0] <= -2 or \
-        #    self.atoms.positions[idx, 1] >= 11 or self.atoms.positions[idx, 1] <= -2 or \
-        #    self.atoms.positions[idx, 2] >= 19 or self.atoms.positions[idx, 2] <= 8:
-        #     self.atoms.positions[idx] -= move
-        if self.atoms.positions[idx, 0] >= 7 or self.atoms.positions[idx, 0] <= -1 or \
-           self.atoms.positions[idx, 1] >= 5 or self.atoms.positions[idx, 1] <= -1 or \
-           self.atoms.positions[idx, 2] >= 18 or self.atoms.positions[idx, 2] <= 9:
-            self.atoms.positions[idx] -= move
+#         curr_pos = self.current_positions()
+        self.atoms.positions[self.free_atoms[idx]] += move
+
 
     def reset(self):
         self.__init__()
@@ -141,12 +138,14 @@ class SurfaceEnv(Environment):
     def __init__(self, max_timesteps):
         self._lattice = Surface()
         self.curr_energy = self.get_energy()
+        self.initial_energy = self.get_energy()
         self.max_timesteps = max_timesteps
         self.terminal = False
         self.steps = 0
-        self.state = np.empty(24)
-        self.positions = np.empty((max_timesteps, 24))
+        self.state = np.empty(self._lattice.num_free_atoms()*3)
+        self.positions = np.empty((max_timesteps, self._lattice.num_free_atoms()*3))
         self.final_energy = []
+        self.init_atoms = self._lattice.atoms.copy()
     
     def get_state(self):
         return self._lattice.current_positions()
@@ -164,12 +163,13 @@ class SurfaceEnv(Environment):
     @property
     def actions(self):
         # return dict(num_actions=32+len(ACTION_NAMES), type='int')
-        return dict(num_actions=8*len(ACTION_NAMES), type='int')
+        return dict(num_actions=self._lattice.num_free_atoms()*len(ACTION_NAMES)+1, type='int')
 
     def reset(self):
         self._lattice.reset()
         self.terminal = False
         self.steps = 0
+        self.max_energy = 0.
         self.positions = np.empty((self.max_timesteps, 24))
         self.state = self.get_state()
         self.positions[self.steps,:] = self.state
@@ -179,36 +179,63 @@ class SurfaceEnv(Environment):
     def do_action(self, action):
         atom_idx = action // len(ACTION_DIRECTION)
         act_idx = action % len(ACTION_DIRECTION)
-        # atom_select = action[:32]
-        # atom_idx = np.argmax(atom_select)
-        # action_select = action[32:]
-        # act_idx = np.argmax(action_select)
-        move = 0.2 * ACTION_DIRECTION[act_idx]
-        self._lattice.change_positions(atom_idx, move)
+        
+        if action==self.actions['num_actions']-1:
+            1
+            dyn = BFGSLineSearch(atoms=self._lattice.atoms, logfile=None)
+            dyn.run(0.2)
+        else:
+            if act_idx == 6:
+                force = self._lattice.atoms.get_forces()[self._lattice.free_atoms,:]
+                move = -0.1*force[atom_idx]
+            if act_idx == 7:
+                force = self._lattice.atoms.get_forces()[self._lattice.free_atoms,:]
+                move = 0.1*force[atom_idx]
+            else:
+                move = 0.2 * ACTION_DIRECTION[act_idx]
+
+            self._lattice.change_positions(atom_idx, move)
         after_energy = self.get_energy()
         return after_energy
 
     def execute(self, action):
         self.steps += 1
         reward = 0
-
-        after_energy = self.do_action(action)
-        # reward = -after_energy
-        # self.curr_energy = after_energy
-        # reward = -self.curr_energy * 0.1
-        # reward = -(after_energy - self.init_energy) * 0.001
-        reward = -after_energy * 0.002
-        self.curr_energy = after_energy
-
+                
         # Terminal
         if self.steps >= self.max_timesteps - 1:
+            
+            dyn = BFGSLineSearch(atoms=self._lattice.atoms, logfile=None)
+            dyn.run(0.1)
+            self.curr_energy = self.get_energy()
+            
             print('Episode ends and final energy:', self.curr_energy)
+        
             self.terminal = True
             self.final_energy.append(self.curr_energy)
+            
+            
             # reward *= 100
             # reward = -after_energy
             # reward = -self.curr_energy * 0.5
-            reward += -self.curr_energy * 0.1
+            reward += -(self.curr_energy-self.initial_energy)*1000
+              
+        else:
+            after_energy = self.do_action(action)
+            # reward = -after_energy
+            # self.curr_energy = after_energy
+            # reward = -self.curr_energy * 0.1
+#             reward = -(after_energy - self.init_energy) * 0.001
+#            reward = -after_energy * 0.002
+#
+#             reward += np.linalg.norm(min_diff(self.init_atoms, self._lattice.atoms))*0.001
+            self.curr_energy = after_energy
+    
+        #penalize the transition state
+        if self.curr_energy > self.max_energy:
+            reward+=-(self.curr_energy-self.max_energy)
+            self.max_energy = self.curr_energy
+#             print(self.max_energy)
         
         # reward += (self.init_energy - self.curr_energy) * 0.001
         # print("state diff:", np.sum(self.state - self.get_state()))
@@ -221,4 +248,21 @@ class SurfaceEnv(Environment):
     def visualize(self):
         self._lattice.visualize()
         # view(self._lattice.atoms)
+        
+def min_diff(atoms_init, atoms_final):
+    positions = (atoms_final.positions-atoms_init.positions)
+
+    fractional = np.linalg.solve(atoms_init.get_cell(complete=True).T,
+                                     positions.T).T
+    if True:
+        for i, periodic in enumerate(atoms_init.pbc):
+            if periodic:
+                # Yes, we need to do it twice.
+                # See the scaled_positions.py test.
+                fractional[:, i] %= 1.0
+                fractional[:, i] %= 1.0
+                
+    fractional[fractional>0.5]-=1
+    return np.matmul(fractional,atoms_init.get_cell(complete=True).T)
+
 
