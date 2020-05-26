3c1089b3b3a0017c6120d756031dc5db6e044ea3
Zack <zulissi@gmail.com>
2020-05-26 10:08:04 -0400

    clean repo, add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_packages
+from distutils.command.install import INSTALL_SCHEMES
+
+
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+setup(name='drl_surface_seg',
+      version='0.0.1',
+      description='Module for DRL on surface segregation systems',
+      url='https://github.com/ulissigroup/surface-seg',
+      author='Jun Yoon, Yuyang, Amir Barati Farimani, Zack Ulissi',
+      author_email='zulissi@andrew.cmu.edu',
+      license='GPL',
+      platforms=[],
+      packages=find_packages(),
+      scripts=[],
+      include_package_data=False,
+      install_requires=['tensorforce','ase>3.19.1','sella @ git+https://github.com/zadorlab/sella.git'],
+      long_description='''Module for generating random catalyst adsorption configurations for high-throughput dataset generation.''',)
