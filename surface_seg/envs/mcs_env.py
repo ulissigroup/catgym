e8d6cc26f28dbaa68ae980d340b59b7624c2fe57
Zachary Ulissi <zulissi@gmail.com>
2020-05-27 15:41:10 -0700

    add energy/forces to state, and make the movement continuous

--- surface_seg/envs/mcs_env.py
+++ surface_seg/envs/mcs_env.py
@@ -22,14 +22,6 @@ MOVE_ACTION_NAMES = [
     'forward',
     'backward']
 
-MOVE_ACTION = np.stack([
-    np.array([0, 0, 1]),
-    np.array([0, 0, -1]),
-    np.array([1, 0, 0]),
-    np.array([-1, 0, 0]),
-    np.array([0, 1, 0]),
-    np.array([0, -1, 0])])
-
 ACTION_LOOKUP = [
     'move',
     'minimize',
@@ -46,7 +38,7 @@ class MCSEnv(gym.Env):
     def __init__(self, size=(2, 2, 4),
                  element_choices={'Ag': 6, 'Au': 5, 'Cu': 5},
                  permute_seed=42,
-                step_size=0.1):
+                 step_size=0.1):
 
         self.step_size=step_size
         
@@ -61,26 +53,26 @@ class MCSEnv(gym.Env):
         self.reset()
 
         # Define the possible actions
-        self.action_space = spaces.Tuple((spaces.Discrete(len(ACTION_LOOKUP)),
-                                          spaces.Discrete(
-                                              len(self.free_atoms)),
-                                          spaces.Discrete(len(MOVE_ACTION))))
+        self.action_space = spaces.Dict({'action_type':spaces.Discrete(len(ACTION_LOOKUP)),
+                                         'atom_selection': spaces.Discrete(
+                                                                      len(self.free_atoms)),
+                                         'movement':spaces.Box(low=-self.step_size,
+                                                               high=self.step_size,
+                                                               shape=(1,3))})
+        
 
-        self.observation_space = spaces.Box(low=0,
-                                            high=1,
-                                            shape=self._get_state().shape)  
+        self.observation_space = self._get_state_space()
 
         return
 
     # open AI gym API requirements
     def step(self, action):
 
-        action_type = ACTION_LOOKUP[action[0]]
+        action_type = ACTION_LOOKUP[action['action_type']]
 
         if action_type == 'move':
-            atom_index = action[1]
-            move_index = action[2]
-            self._move_atom(atom_index, move_index)
+            self._move_atom(action['atom_selection'], 
+                            action['movement'])
 
         elif action_type == 'minimize':
             self._minimize()
@@ -89,25 +81,22 @@ class MCSEnv(gym.Env):
             self._transition_state_search()
 
         elif action_type == 'steepest_descent':
-            atom_index = action[1]
-            self._steepest_descent(atom_index)
+            self._steepest_descent(action['atom_selection'])
 
         elif action_type == 'steepest_ascent':
-            atom_index = action[1]
-            self._steepest_ascent(atom_index)
+            self._steepest_ascent(action['atom_selection'])
 
         else:
             raise Exception('I am not sure what action you mean!')
 
-        self.atoms.wrap()
-        
+        relative_energy = self._get_relative_energy()
+
         observation = self._get_state()
-        
-        relative_energy = self.atoms.get_potential_energy() - self.initial_energy
 
         reward = self._get_reward(relative_energy)
         
-        if relative_energy > 2:
+        #Stop if relative energy gets too high
+        if relative_energy > 2.0:
             episode_over = True
         else:
             episode_over = False
@@ -126,8 +115,8 @@ class MCSEnv(gym.Env):
         if mode=='rgb_array':
             # return an rgb array representing the picture of the atoms
             return render_image(self.atoms.repeat((2,2,1)), rotation='48x,-51y,-144z', bbox=(-10,0,10,20))
-#         return
-    
+        else:
+            return
     
     def close(self):
         return
@@ -162,6 +151,9 @@ class MCSEnv(gym.Env):
         dyn.run(1e-2, steps=10)
 
         return
+    
+    def _get_relative_energy(self):
+        return self.atoms.get_potential_energy() - self.initial_energy
 
     def _steepest_descent(self, atom_index):
         force = self.atoms.get_forces()[self.free_atoms[atom_index], :]
@@ -175,16 +167,30 @@ class MCSEnv(gym.Env):
         self.atoms.positions[self.free_atoms[atom_index]] += move
         return
 
-    def _move_atom(self, atom_index, move_index):
+    def _move_atom(self, atom_index, movement):
         # Helper function to move an atom
-        self.atoms.positions[self.free_atoms[atom_index]
-                             ] += MOVE_ACTION[move_index]*self.step_size
+        self.atoms.positions[self.free_atoms[atom_index]] \
+                        += movement.reshape((3,))
         return
 
     def _get_state(self):
         # helper function to get the current state space, which is just the position
         # of the free atoms as one long vector (should be improved)
-        return self.atoms.get_scaled_positions()[self.free_atoms]
+        return {'positions': self.atoms.get_scaled_positions()[self.free_atoms],
+                'energy':np.array([self._get_relative_energy()]),
+               'forces':self.atoms.get_forces()[self.free_atoms, :]}
+    
+    def _get_state_space(self):
+        return spaces.Dict({'positions': spaces.Box(low=0,
+                                            high=1,
+                                            shape=self.atoms.get_scaled_positions()[self.free_atoms].shape),
+                                               'energy': spaces.Box(low=-2,
+                                            high=2,
+                                            shape=(1,)),
+                           'forces': spaces.Box(low=-2,
+                                            high=2,
+                                            shape=self.atoms.get_scaled_positions()[self.free_atoms].shape)})
+
 
     def _generate_slab(self, size, element_choices, permute_seed):
         # generate a pseudo-random sequence of elements
